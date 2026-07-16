54c0711e22aafbdd103f0a21bf8615237548a146
Miriam <mirimz21@hotmail.com>
2026-07-16 16:03:16 +0200

    constructor para crear varios libros y meterlos en un array. imprimir el título de cada uno

--- Automatización JS/library.js
+++ Automatización JS/library.js
@@ -4,6 +4,30 @@ function Book (title, author, numPages, isRead) {
     this.numPages = numPages;
     this.isRead = isRead;
     this.info = function info() {
-     console.log(`${this.title} by ${this.author}, ${this.numPages} pages, ${this.isRead}`);
+        console.log(`${this.title} by ${this.author}, ${this.numPages} pages, ${this.isRead}`);
+    };
+    this.id = crypto.randomUUID();
+};
+
+//todos los libros se van a almacenar en este array
+const myLibrary = [];
+
+function addBookToLibrary(title, author, numPages, isRead) {
+    //coger parámetros, crear un libro y almacenarlo en el array myLibrary
+    const libro = new Book(title, author, numPages, isRead);
+    myLibrary.push(libro);
+};
+
+addBookToLibrary("El Hobbit", "J.R.R. Tolkien", 450, "no leído todavía");
+addBookToLibrary("El señor de los anillos", "J.R.R. Tolkien", 1200, "leído");
+addBookToLibrary("Harry Potter", "J.K. Rowling", 300, "leído");
+addBookToLibrary("El triángulo de hielo", "Marcos Nieto", 520, "leído");
+addBookToLibrary("El juego de Ender", "Orson Scott Card", 400, "no leído todavía");
+
+function titulo(myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i].title);
     }
 };
+
+titulo(myLibrary);
