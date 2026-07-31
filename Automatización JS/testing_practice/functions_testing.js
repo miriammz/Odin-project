b286b8df1dcbd352858b797008284d6f0b3b8578
Miriam <mirimz21@hotmail.com>
2026-07-31 18:26:23 +0200

    añadidos tests y objeto para calculadora

--- Automatización JS/testing_practice/functions_testing.js
+++ Automatización JS/testing_practice/functions_testing.js
@@ -7,4 +7,20 @@ function reverseString(string) {
     string = (string.split('')).reverse();
     return string.join('');
 }
-module.exports = {capitalize, reverseString};
+
+const calculator = (() => {
+    const add = (a, b) => {
+        return a + b;
+    };
+    const subtract = (a, b) => {
+        return a - b;
+    };
+    const multiply = (a,b) => {
+        return a*b;
+    };
+    const divide = (a,b) => {
+        return a/b;
+    };
+    return {add, subtract, multiply, divide};
+})();
+module.exports = {capitalize, reverseString, calculator};
--- Automatización JS/testing_practice/random_testing.test.js
+++ Automatización JS/testing_practice/random_testing.test.js
@@ -1,8 +1,23 @@
-const {capitalize, reverseString} = require('./functions_testing');
+const {capitalize, reverseString, calculator} = require('./functions_testing');
 test('primera letra mayúscula', () => {
     expect(capitalize("casa")).toBe("Casa");
 });
 
 test('cambio de orden', () => {
     expect(reverseString("casa")).toBe("asac");
+});
+
+describe('calculator', () => {
+    test('suma', () => {
+        expect(calculator.add(3,4)).toBe(7);
+    });
+    test('resta', () => {
+        expect(calculator.subtract(4,3)).toBe(1);
+    });
+    test('multiplicación', () => {
+        expect(calculator.multiply(3,4)).toBe(12);
+    });
+    test('división', () => {
+        expect(calculator.divide(3,1)).toBe(3);
+    });
 });
