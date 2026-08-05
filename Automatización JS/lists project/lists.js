6b4a5c72ce706e4823e117f95106d819834dbbd9
Miriam <mirimz21@hotmail.com>
2026-08-05 19:41:14 +0200

    añadidos métodos at y pop

--- Automatización JS/lists project/lists.js
+++ Automatización JS/lists project/lists.js
@@ -65,4 +65,38 @@ class LinkedList {
         }
         return value;
     }
+
+    //devuelve el valor del nodo dado
+    at(index) {
+        let value;
+        if (index >= this.length) {
+            value = undefined;
+        } else {
+            let currentNode = this.headProp;
+            for (let i = 0; i < index; i++) {
+                currentNode = currentNode.nextNode;
+            }
+            value = currentNode.value;
+        }
+        return value;
+    }
+
+    //elimina head y devuelve el valor que tiene
+    pop() {
+        let value;
+        if (this.length === 0) {
+            value = undefined;
+        } else if (this.length === 1) {
+            value = this.headProp.value;
+            this.headProp = null;
+            this.tailProp = null;
+            this.length = 0;
+        } else {
+            value = this.headProp.value;
+            let newHeadNode = this.headProp.nextNode;
+            this.headProp = newHeadNode;
+            this.length = this.length - 1;
+        }
+        return value;
+    }
 }
