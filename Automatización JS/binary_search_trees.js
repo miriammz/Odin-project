55ee397540da1764e178d0380ef13c7f90e32b22
Miriam <mirimz21@hotmail.com>
2026-08-10 17:34:58 +0200

    proyect binary search trees, clases y primera función creada para ordenar y quitar duplicados y construir el array definitivo

---
+++ Automatización JS/binary_search_trees.js
@@ -0,0 +1,35 @@
+import { array } from "node:stream/iter";
+
+class Node {
+    constructor(data, nodeLeft = null, nodeRight = null) {
+        this.data = data;
+        this.nodeLeft = nodeLeft;
+        this.nodeRight = nodeRight;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.array = this.#sortAndRemoveDup(array);
+        this.root = this.#buildTree(this.array);
+    }
+
+    #sortAndRemoveDup(array) {
+        array = [...new Set(array)];
+        let orden = array.sort((a, b) => a - b);
+        return orden;
+    }
+
+    #buildTree(array) {
+        let node = new Node();
+        if (array.length === 0) {
+            return null;
+        } else {
+            let medio = Math.floor(array.length / 2);
+            node.data = array[medio];
+            node.nodeLeft = this.#buildTree(array.slice(0, medio));
+            node.nodeRight = this.#buildTree(array.slice(medio + 1));
+            return node;
+        }
+    }
+}
