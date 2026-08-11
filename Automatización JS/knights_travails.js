39f16a848d0221ba9e07b96aae477f27a1fc6ab7
Miriam <mirimz21@hotmail.com>
2026-08-11 18:25:58 +0200

    completada la función principal

--- Automatización JS/knights_travails.js
+++ Automatización JS/knights_travails.js
@@ -5,17 +5,34 @@ function getValidMoves(position) {
     for (let i = 0; i < movements.length; i++) {
         array.push([movements[i][0] + position[0], movements[i][1] + position[1]]);
     }
-    console.log(array)
 
     //descartar resultados fuera del tablero (fuera de 0 - 7)
     let validMoves;
     validMoves = array.filter(([x,y]) => x >= 0 && x <= 7 && y >= 0 && y <= 7);
-    console.log(validMoves)
     return validMoves;
 }
 
-function KnightMoves(start, end) {
-    getValidMoves([7,7]);
+function knightMoves(start, end) {
+    let queue = [[start]];
+    let visited = new Set([start.toString()]);
+    while (queue.length > 0) {
+        let first = queue.shift();
+        let last = first.at(-1);
+        if (last.toString() === end.toString()) {
+            console.log(`You made it in ${first.length - 1} moves! Here's your path: `);
+            first.forEach(pos => console.log(`[${pos}]`));
+            return;
+        } else {
+            let valid = getValidMoves(last);
+            for (let i = 0; i < valid.length; i++) {
+                if (!visited.has(valid[i].toString())) {
+                    visited.add(valid[i].toString());
+                    let newArray = [...first, valid[i]];
+                    queue.push(newArray);
+                }
+            }
+        }
+    }
 }
 
-KnightMoves([0,0],[1,2]);
+knightMoves([2,3],[4,3]);
